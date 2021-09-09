b6cd3eeb46a35f3f5f214fe3299b5d481ac624d4
Patrick Kinimond Hassø <89964910+Kinimond@users.noreply.github.com>
2021-09-09 12:46:44 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+//Exercise 4
+    public static int testing (int tester) {
+
+        return tester;
+    }
+    public static void main(String[] args) {
+        int tester = testing(7357);
+        System.out.println(tester);
+
+    }
+}
